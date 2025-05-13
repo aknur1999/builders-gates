7052a260fee47b423246ae7f7b173b737ea3396f
aknur1999 <kae.aknur@gmail.com>
2025-05-13 13:48:25 +0900

    fix: user image

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      'avatars.githubusercontent.com',  // GitHub avatars
+      'github.com',                     // GitHub general 
+    ],
+  },
 };
 
 export default nextConfig;
